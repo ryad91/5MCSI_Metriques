46326838e5a812bd747978a66a7ed47e148c4060
ryad91 <166554961+ryad91@users.noreply.github.com>
2024-11-20 15:00:02 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
-import sqlite3
+import sqlite3#
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
